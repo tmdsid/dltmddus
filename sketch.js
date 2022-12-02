08d3b2a602ea53bd015bcd20fd5957ef62d05fae
tmdsid <119645512+tmdsid@users.noreply.github.com>
2022-12-02 13:56:24 +0900

    4
    4

--- sketch.js
+++ sketch.js
@@ -9,6 +9,26 @@ function setup() {
     background(51);
 }
 
+song.addCue(2, changeBackground, color(0, 0, 255));
+song.addCue(4, changeBackground, color(0, 255, 255));
+song.addCue(6, changeBackground, color(255, 255, 255));
+
+function changeBackground(col) {
+    background(col);
+    }
+    function jumpSong() {
+    var len = song.duration();
+    var t = 0; //random(len);
+    console.log(t);
+    song.jump(t);
+    }
+    function draw() {
+    //if (song.currentTime() > 5) {
+    //background(song.currentTime() * 10, 0, 255);
+    //}
+    }
+
+
 function togglePlaying() {
     if(!song.isPlaying()) {
         song.play();
