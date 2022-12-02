3ca9d27f4a419213f3087be4684a511405cf35ef
tmdsid <119645512+tmdsid@users.noreply.github.com>
2022-12-02 14:36:38 +0900

    8
    8

--- sketch.js
+++ sketch.js
@@ -1,51 +1,26 @@
 var song;
 var button;
-var amp;
-
 
 function setup() {
     createCanvas(200,200);
     song = loadSound('afterlike.mp3', loaded);
     button = createButton('play');
     button.mousePressed(togglePlaying);
-    amp = new p5.Amplitude();
     background(51);
+}
+
+
+function togglePlaying() {
+    if(!song.isPlaying()) {
+        song.play();
+        song.setvolume(0.3);
+        button.html('pause');
+    } else {
+        song.stop();
+        button.html('play');
+    }
 }
 
 function loaded() {
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-    }
-
-function draw() {
-    background(51);
-
-    var vol = amp.getLevel();
-    var diam = map(vol, 0, 0.3, 10, 200);
-
-    fill(255, 0, 255);
-ellipse(width / 2, height / 2, diam, diam);
+    console.log('loaded');
 }
-
-function togglePlaying() {
-    if (!song.isPlaying()) {
-    song.play();
-    song.setVolume(0.3);
-    button.html('pause');
-    } else {
-    song.stop();
-    button.html('play');
-    }
-}
-
-function changeBackground(col) {
-    background(col);
-    }
-    function jumpSong() {
-    var len = song.duration();
-    var t = 0; //random(len);
-    console.log(t);
-    song.jump(t);
-    }
-    function draw() {
-   
